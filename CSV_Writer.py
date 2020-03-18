aa4a2f285c5da06e781938ab2f5008d91611946c
TechFish3000 <42089115+TechFish3000@users.noreply.github.com>
2020-03-18 12:18:21 +1030

    CSV writer i made

---
+++ CSV_Writer.py
@@ -0,0 +1,42 @@
+cols = int(input("How many columns?"))
+entries = 0
+headings = []
+endwrite = []
+temp = ""
+for i in range(1, int(cols) + 1):
+    headings.append(input("Column " + str(i) + " header: "))
+    temp = temp + headings[-1] + ","
+temp = temp[0:-1] + "\n"
+endwrite.append(temp)
+
+ans = ""
+while ans.lower() != "f":
+    if ans != "spec":
+        ans = input("(C)reate a new entry or (F)inish the file or (U)ndo last entry: ")
+    else:
+        ans = "c"
+    if ans.lower() == "c":
+
+        temp = ""
+        for i in range(0, cols):
+            temp = temp + input("Entry " + str(entries) + " value for " + headings[i] + ": ") + ","
+        temp = temp[0:-1] + "\n"
+        endwrite.append(temp)
+        entries = entries + 1
+
+    elif ans.lower() == "u":
+        endwrite.remove(endwrite[-1])
+        print("Entry " + str(entries) + " Deleted.")
+        entries = entries - 1
+    elif ans.lower() == "f":
+        continue
+    else:
+        print("Invalid answer. Defaulting to creation of an entry.")
+        ans = "spec"
+        continue
+
+print("The output .csv file is ready. Provide a name.")
+ans = input(">> ")
+endfile = open(ans + ".csv", "w")
+endfile.writelines(endwrite)
+print(ans + ".csv has been written.")
